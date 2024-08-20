0522597a97f6b81caa05f75e208d6a8a85e8e1fc
MDEESA007 <m.d.eesa007@gmail.com>
2024-08-20 19:42:26 +0530

    Add files via upload

---
+++ hii.java
@@ -0,0 +1,16 @@
+class bye
+{
+  public void eesa()
+  {
+    System.out.println("mohammed eesa");
+  }
+}
+ 
+class hii
+{
+  public static void main(String args[])
+  {
+    bye obj=new bye();
+    obj.eesa();
+  }
+}
