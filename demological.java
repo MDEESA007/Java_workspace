84c4285643b83ac7efe0649a0fead2378cdc6cd0
MDEESA007 <m.d.eesa007@gmail.com>
2024-09-17 13:01:02 +0530

    Add files via upload

---
+++ demological.java
@@ -0,0 +1,17 @@
+class demological
+{
+   public static void main(String args[])
+   {
+      int mark=87;
+      int assmark=15;
+      
+      if(mark>=80 && assmark>=15)
+       {
+         System.out.println("pass");
+       }
+      else
+       {
+         System.out.println("Fail");
+       }
+   }
+}
